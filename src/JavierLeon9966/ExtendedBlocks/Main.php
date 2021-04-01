5d3046dc4b0f2377ccce3c86c6cad5cb83547318
DragonfireHD98 <50929188+DragonfireHD@users.noreply.github.com>
2021-04-02 02:16:48 +0300

    Maybe Fix ? overwrite Block Problem ?
    RuntimeException: "Trying to overwrite an already registered block" (EXCEPTION) in "pmsrc/src/pocketmine/block/BlockFactory" at line 370
    
    [01:11:29] [Server thread/CRITICAL]: #0 plugins/ExtendedBlocks_dev-33.phar/src/JavierLeon9966/ExtendedBlocks/Main(89): pocketmine\block\BlockFactory::registerBlock(object JavierLeon9966\ExtendedBlocks\block\CustomBlock)

--- src/JavierLeon9966/ExtendedBlocks/Main.php
+++ src/JavierLeon9966/ExtendedBlocks/Main.php
@@ -92,7 +92,7 @@ class Main extends PluginBase implements Listener{
 				ItemFactory::addCreativeItem(ItemFactory::get(intval($block['itemId'] ?? 255 - $block['id'])));
 			}
 		}
-		BlockFactory::registerBlock(new NetheriteBlock);
+		BlockFactory::registerBlock(new NetheriteBlock), true);
 		ItemFactory::addCreativeItem(ItemFactory::get(255 - 525));
 	}
 	public function onEnable(){
